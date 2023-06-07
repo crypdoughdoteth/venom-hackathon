1467ffa5cf3b835f589bcd8f1b4a12936d3f3486
crypdough.eth <106392730+crypdoughdoteth@users.noreply.github.com>
2023-06-07 18:39:00 -0400

    Update 1-deploy-sample.ts

--- scripts/1-deploy-sample.ts
+++ scripts/1-deploy-sample.ts
@@ -1,7 +1,7 @@
 async function main() {
   const signer = (await locklift.keystore.getSigner("0"))!;
   const { contract: sample, tx } = await locklift.factory.deployContract({
-    contract: "Sample",
+    contract: "BountyBoard",
     publicKey: signer.publicKey,
     initParams: {
       _nonce: locklift.utils.getRandomNonce(),
